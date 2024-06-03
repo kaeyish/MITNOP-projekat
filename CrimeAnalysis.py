7010483e334a352213d625d3397c520aa129d499
Dusanka Ignjatovic <ignjatovicdusanka02@gmail.com>
2024-06-03 18:48:55 +0200

    pukao keras

--- CrimeAnalysis.py
+++ CrimeAnalysis.py
@@ -4,6 +4,7 @@ Created on Mon May 26 22:25:08 2024
 
 @author: Dušanka
 """
+
 #%% importovi
 import pandas as pd
 import numpy as np
@@ -16,11 +17,13 @@ from mlxtend.frequent_patterns import apriori, association_rules
 
 date_parser = lambda x: parse(x)
 
+
 #%% ucitavanje podataka
 
 path = Path(__file__).parent / "Crime.csv"
 
 df = pd.read_csv(path)
+
 
 print("Shape dataseta pre uklanjanja null: {}", df.shape)
 
@@ -85,7 +88,42 @@ df['Block Address'] = df['Block Address'].astype(str)
 
 
 print ("Oblik dataseta posle razdvajanja kolona: {}",df.shape)
+
+# Prebacivanje kolona za kasnije
+cols = ['Case Number', 'Occurred From Date', 'Reported Date', 'Crime Type',
+        'Block Address', 'Occurred Time', 'Occurred Date', 'Occurred Day',
+        'Occurred Month', 'Occurred Year', 'Reported Time']
+
+new_cols_order = ['Case Number', 'Occurred From Date', 'Reported Date', 'Crime Type',
+                  'Block Address', 'Occurred Date', 'Occurred Time', 'Occurred Day',
+                  'Occurred Month', 'Occurred Year', 'Reported Time']
+
+df = df[new_cols_order]
+
+
+
 print (df.columns)
+
+
+
+#%% Mapiranje string podataka na integere
+
+# Mapiranje tipova podataka
+print("\nUnique values:\n", np.unique(df['Crime Type']))
+print("TOTAL:", len(np.unique(df['Crime Type'])))
+
+mapping_dict = {'ARSON': 1, 'ASSAULT': 2, 'BURGLARY': 3, 'DUI ARREST': 4, 
+                'ROBBERY': 5, 'SHOPLIFTING': 6, 
+                'THEFT': 7, 'VANDALISM': 8}
+
+print ("********************")
+
+# Apply the mapping to the dataset
+df['Crime Type'] = df['Crime Type'].map(mapping_dict)
+
+print("\nUnique values:\n", np.unique(df['Crime Type']))
+print("TOTAL:", len(np.unique(df['Crime Type'])))
+
 
 #%% 2024 CNT 
 
@@ -108,26 +146,15 @@ print("PRE RESHAPE: \n\n",train_data)
 
 #%% Preprocessing za trening skup
 # Preoblikovanje trening skupa iz 1d niza u 2d niz
+
 dataset_train = train_data['Crime Type'].values 
 dataset_train = np.reshape(dataset_train, (-1,1)) 
 print("\n\nPOSLE RESHAPE: \n\n", dataset_train)
 
-# Mapiranje tipova podataka
-print("\nUnique values:\n", np.unique(dataset_train))
-print("TOTAL:", len(np.unique(dataset_train)))
-
-mapping_dict = {'ARSON': 1, 'ASSAULT': 2, 'BURGLARY': 3, 'DUI ARREST': 4, 
-                'ROBBERY': 5, 'SHOPLIFTING': 6, 
-                'THEFT': 7, 'VANDALISM': 8}
-
-# print ("\\\\\\\ PROSAO \\\\")
-
-# Apply the mapping to the dataset
-df['Crime Type'] = df['Crime Type'].map(mapping_dict)
 
 print("\nUnique values:\n", np.unique(dataset_train))
 
-
+#%%
 # Normalizacija trening skupa
 from sklearn.preprocessing import MinMaxScaler
 scaler = MinMaxScaler(feature_range=(0,1))
@@ -184,7 +211,209 @@ X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1],1))
 y_test = np.reshape(y_test, (y_test.shape[0],1))
 print("X_test :",X_test.shape,"y_test :",y_test.shape)
 
+
+#%% Ucitavanje biblioteka za neuronsku mrezu
+
+from keras.models import Sequential
+from keras.layers import LSTM
+from keras.layers import Dense
+from keras.layers import SimpleRNN
+from keras.layers import Dropout
+from keras.layers import GRU, Bidirectional
+from keras.optimizers import SGD
+from sklearn import metrics
+from sklearn.metrics import mean_squared_error
+
+#%%
+
+
+from scikeras.wrappers import KerasRegressor
+from sklearn.model_selection import GridSearchCV
+
+# Function to create model, required for KerasRegressor
+def create_model(optimizer='adam', activation='relu'):
+    # create model
+    model = Sequential()
+    model.add(SimpleRNN(50, activation=activation, return_sequences=True, input_shape=(X_train.shape[1],1)))
+    model.add(Dropout(0.2))
+    model.add(SimpleRNN(50, activation=activation, return_sequences=True))
+    model.add(SimpleRNN(50, activation=activation, return_sequences=True))
+    model.add(SimpleRNN(50, activation=activation, return_sequences=True))
+    model.add(SimpleRNN(50, activation=activation, return_sequences=True))
+    model.add(SimpleRNN(50, activation=activation, return_sequences=True))
+    model.add(SimpleRNN(50, activation=activation, return_sequences=True))
+    model.add(SimpleRNN(50, activation=activation))
+    model.add(Dense(1, activation='sigmoid'))
+    # Compile model
+    model.compile(loss='mean_squared_error', optimizer=optimizer)
+    return model
+
+# create model
+model = KerasRegressor(build_fn=create_model, verbose=0)
+
+# define the grid search parameters
+optimizer = ['SGD', 'RMSprop', 'Adagrad', 'Adadelta', 'Adam', 'Adamax', 'Nadam']
+activation = ['softmax', 'softplus', 'softsign', 'relu', 'tanh', 'sigmoid', 'hard_sigmoid', 'linear']
+param_grid = dict(optimizer=optimizer, activation=activation)
+grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=-1, cv=3)
+grid_result = grid.fit(X_train, y_train)
+
+# summarize results
+print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+means = grid_result.cv_results_['mean_test_score']
+stds = grid_result.cv_results_['std_test_score']
+params = grid_result.cv_results_['params']
+for mean, stdev, param in zip(means, stds, params):
+    print("%f (%f) with: %r" % (mean, stdev, param))
+
 #%% Treniranje 
+# initializing the RNN
+regressor = Sequential()
+
+# adding RNN layers and dropout regularization
+regressor.add(SimpleRNN(units = 50, 
+						activation = "tanh",
+						return_sequences = True,
+						input_shape = (X_train.shape[1],1)))
+regressor.add(Dropout(0.2))
+
+regressor.add(SimpleRNN(units = 50, 
+						activation = "tanh",
+						return_sequences = True))
+
+regressor.add(SimpleRNN(units = 50,
+						activation = "tanh",
+						return_sequences = True))
+
+regressor.add(SimpleRNN(units = 50, 
+						activation = "tanh",
+						return_sequences = True))
+
+regressor.add(SimpleRNN(units = 50,
+						activation = "tanh",
+						return_sequences = True))
+
+regressor.add(SimpleRNN(units = 50, 
+						activation = "tanh",
+						return_sequences = True))
+
+regressor.add(SimpleRNN(units = 50,
+						activation = "tanh",
+						return_sequences = True))
+
+
+regressor.add( SimpleRNN(units = 50))
+
+# adding the output layer
+regressor.add(Dense(units = 1,activation='sigmoid'))
+
+# compiling RNN
+regressor.compile(optimizer = SGD(learning_rate=0.01,
+								momentum=0.9, 
+								nesterov=True), 
+				loss = "mean_squared_error")
+
+# fitting the model
+regressor.fit(X_train, y_train, epochs = 10, batch_size = 32)
+regressor.summary()
+
+#%% LSTM Model
+#Initialising the model
+regressorLSTM = Sequential()
+
+#Adding LSTM layers
+regressorLSTM.add(LSTM(50, 
+					return_sequences = True, 
+					input_shape = (X_train.shape[1],1)))
+regressorLSTM.add(LSTM(50, 
+					return_sequences = False))
+regressorLSTM.add(Dense(25))
+
+#Adding the output layer
+regressorLSTM.add(Dense(1))
+
+#Compiling the model
+regressorLSTM.compile(optimizer = 'adam',
+					loss = 'mean_squared_error',
+					metrics = ["accuracy"])
+
+#Fitting the model
+regressorLSTM.fit(X_train, 
+				y_train, 
+				batch_size = 32, 
+				epochs = 12)
+regressorLSTM.summary()
+#%% GRU
+
+#Initialising the model
+regressorGRU = Sequential()
+
+# GRU layers with Dropout regularisation
+regressorGRU.add(GRU(units=50, 
+					return_sequences=True,
+					input_shape=(X_train.shape[1],1),
+					activation='tanh'))
+regressorGRU.add(Dropout(0.2))
+
+regressorGRU.add(GRU(units=50, 
+					return_sequences=True,
+					activation='tanh'))
+
+
+# The output layer
+regressorGRU.add(Dense(units=1,
+					activation='relu'))
+# Compiling the RNN
+regressorGRU.compile(optimizer = 'adam',
+					loss = 'mean_squared_error',
+					metrics = ["accuracy"])
+
+# Fitting the data
+regressorGRU.fit(X_train,y_train,epochs=20,batch_size=64)
+regressorGRU.summary()
+
+#%% Testiranje
+y_RNN = regressor.predict(X_test)
+y_LSTM = regressorLSTM.predict(X_test)
+y_GRU = regressorGRU.predict(X_test)
+
+y_RNN_O = scaler.inverse_transform(y_RNN) 
+y_LSTM_O = scaler.inverse_transform(y_LSTM) 
+y_GRU_O = scaler.inverse_transform(y_GRU)
+
+#%% Vizualizacija
+
+import matplotlib.pyplot as plt
+
+fig, axs = plt.subplots(3,figsize =(18,12),sharex=True, sharey=True)
+fig.suptitle('Model Predictions')
+
+#Simple RNN Plot
+axs[2].plot(train_data.index[150:], train_data['Crime Type'][150:], label = "train_data", color = "b")
+axs[2].plot(test_data.index, train_data['Crime Type'], label = "test_data", color = "g")
+axs[2].plot(test_data.index[50:], y_RNN_O, label = "y_RNN", color = "brown")
+axs[2].legend()
+axs[2].title.set_text("Basic RNN")
+
+#Plot for LSTM predictions
+#axs[0].plot(train_data.index[150:], train_data['Crime Type'][150:], label = "train_data", color = "b")
+axs[0].plot(test_data.index, test_data['Crime Type'], label = "test_data", color = "g")
+axs[0].plot(test_data.index[50:], y_LSTM_O, label = "y_LSTM", color = "orange")
+axs[0].legend()
+axs[0].title.set_text("LSTM")
+
+#Plot for GRU predictions
+#axs[1].plot(train_data.index[150:], train_data['Crime Type'][150:], label = "train_data", color = "b")
+axs[1].plot(test_data.index, test_data['Crime Type'], label = "test_data", color = "g")
+axs[1].plot(test_data.index[50:], y_GRU_O, label = "y_GRU", color = "red")
+axs[1].legend()
+axs[1].title.set_text("GRU")
+
+plt.xlabel("Days")
+plt.ylabel("Crime Rate")
+
+plt.show()
+
 
 
 
